28c0a369ba1b6ed9afb62f94ab6ad1138693a5b0
admidere <admasud89@gmail.com>
2023-03-22 11:18:30 +0000

    airbnb

--- models/place.py
+++ models/place.py
@@ -2,7 +2,7 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Float, Integer, ForeignKey, Table
-
+from os import environ
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -18,3 +18,21 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        reviews = relationship('Review',
+                              cascade='all, delete', backref='place')
+                    
+        amenities = relationship('Amenity', backref='place_amenities',
+                                secondary='place_amenity',
+                                viewonly=False)
+    else:
+        @property
+        def reviews(self):
+            """ getter returns list of reviews """
+            list_of_reviews = []
+            all_reviews = models.strage.all(Review)
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    list_of_reviews.append(review)
+            return list_of_reviews
